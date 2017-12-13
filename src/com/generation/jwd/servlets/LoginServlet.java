7f1cb6cf6c61553a6fbdd0611bb745bb03275ce6
Arranque 1 <Arranque 1@a103-pc06>
2017-12-13 09:47:48 +0100

    Comment on Servlets

--- src/com/generation/jwd/servlets/LoginServlet.java
+++ src/com/generation/jwd/servlets/LoginServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//Hola
 /**
  * Servlet implementation class LoginServlet
  */
